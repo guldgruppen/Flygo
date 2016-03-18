96a0c7d6a2d202ce1b449a2cccdf567c5369bd53
Kasper Due <kasp9023@hotmail.com>
2016-03-18 19:56:10 +0100

    HomePage init

---
+++ FlygoApp/FlygoApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using FlygoApp.Annotations;
+using FlygoApp.Commons;
+
+namespace FlygoApp.ViewModels
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        #region Instance Fields
+        private ICommand _openMenuCommand;
+        private bool _menuOpen;
+
+        #endregion
+        #region Properties
+        public ICommand OpenMenuCommand
+        {
+            get
+            {
+                return _openMenuCommand ?? (_openMenuCommand = new RelayCommand(() =>
+                { MenuOpen = !MenuOpen; }
+                ));
+            }
+            set { _openMenuCommand = value; }
+        }
+        public bool MenuOpen
+        {
+            get { return _menuOpen; }
+            set
+            {
+                _menuOpen = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomePageViewModel()
+        {
+            
+        }
+        #endregion
+        #region Notify Changed Region
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
